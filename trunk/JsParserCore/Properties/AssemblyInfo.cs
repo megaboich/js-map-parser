b0869818d821ca092e23042e4830a76ae172b575
mega.boich <mega.boich@a7d6c96a-3943-11de-a15a-d1801d6fabe8>
2010-05-03 15:39:07 +0000

    [copyright] [new images]
    git-svn-id: https://js-addin.googlecode.com/svn@37 a7d6c96a-3943-11de-a15a-d1801d6fabe8

--- trunk/JsParserCore/Properties/AssemblyInfo.cs
+++ trunk/JsParserCore/Properties/AssemblyInfo.cs
@@ -8,9 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JsParserCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("JsParserCore")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Alexander Boyko 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
