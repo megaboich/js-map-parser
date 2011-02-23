49bb404eece00e7cc8b2763a9084f394bf933497
mega.boich <mega.boich@a7d6c96a-3943-11de-a15a-d1801d6fabe8>
2011-02-23 09:21:04 +0000

    [add] missing file
    git-svn-id: https://js-addin.googlecode.com/svn@64 a7d6c96a-3943-11de-a15a-d1801d6fabe8

---
+++ trunk/JsParserCore/UI/ExpandedNodesManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsParserCore.UI
+{
+	class ExpandedNodesManager
+	{
+		private Dictionary<string, Dictionary<string, bool>> _storage = new Dictionary<string, Dictionary<string, bool>>();
+		private string _activeDocName;
+		private Dictionary<string, bool> _activeStorage;
+
+		public string ActiveDocumentName 
+		{
+			get { return _activeDocName; }
+			set
+			{
+				_activeDocName = value;
+				if (!HasDocumentInStorage(_activeDocName))
+				{
+					_storage.Add(_activeDocName, new Dictionary<string, bool>());
+				}
+
+				_activeStorage = _storage[ActiveDocumentName];
+			}
+		}
+
+		public bool HasDocumentInStorage(string docName)
+		{
+			return _storage.ContainsKey(docName);
+		}
+
+		public Dictionary<string, bool> ActiveStorage
+		{
+			get
+			{
+				return _activeStorage;
+			}
+		}
+
+		public bool IsNoteExpanded(CustomTreeNode node)
+		{
+			var storage = ActiveStorage;
+			var nodeKey = GetNodeKey(node);
+			if (storage.ContainsKey(nodeKey))
+			{
+				return storage[nodeKey];
+			}
+
+			return false;
+		}
+
+		public void SetExpandedState(CustomTreeNode node)
+		{
+			ActiveStorage[GetNodeKey(node)] = node.IsExpanded;
+		}
+
+		private string GetNodeKey(CustomTreeNode node)
+		{
+			return node.CodeNode.StartLine + "_(" + node.Text + ")";
+		}
+	}
+}
