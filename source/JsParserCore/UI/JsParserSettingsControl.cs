c3cb27413f89d287030bcfe0b051ad976c32f503
mega.boich@gmail.com <mega.boich@gmail.com@a7d6c96a-3943-11de-a15a-d1801d6fabe8>
2012-01-12 14:39:14 +0000

    Refactor UI for options settings toolwindow - move UI to User Control
    git-svn-id: https://js-addin.googlecode.com/svn@137 a7d6c96a-3943-11de-a15a-d1801d6fabe8

---
+++ source/JsParserCore/UI/JsParserSettingsControl.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using JsParserCore.Helpers;
+using JsParserCore.Properties;
+
+namespace JsParserCore.UI
+{
+    public partial class JsParserSettingsControl : UserControl
+    {
+        private Font _defaultTreeFont;
+
+        public JsParserSettingsControl()
+        {
+            InitializeComponent();
+
+            _defaultTreeFont = Font;
+        }
+
+        public void InitSettings()
+        {
+            chTrackActiveItem.Checked = Settings.Default.TrackActiveItem;
+            edExtensions.Lines = Settings.Default.Extensions.OfType<string>().ToArray();
+
+            taggedFuncLabel1.ForeColor = Settings.Default.taggedFunction1Color;
+            taggedFuncLabel1.Font = Settings.Default.taggedFunction1Font ?? _defaultTreeFont;
+            taggedFuncLabel2.ForeColor = Settings.Default.taggedFunction2Color;
+            taggedFuncLabel2.Font = Settings.Default.taggedFunction2Font ?? _defaultTreeFont;
+            taggedFuncLabel3.ForeColor = Settings.Default.taggedFunction3Color;
+            taggedFuncLabel3.Font = Settings.Default.taggedFunction3Font ?? _defaultTreeFont;
+            taggedFuncLabel4.ForeColor = Settings.Default.taggedFunction4Color;
+            taggedFuncLabel4.Font = Settings.Default.taggedFunction4Font ?? _defaultTreeFont;
+            taggedFuncLabel5.ForeColor = Settings.Default.taggedFunction5Color;
+            taggedFuncLabel5.Font = Settings.Default.taggedFunction5Font ?? _defaultTreeFont;
+            taggedFuncLabel6.ForeColor = Settings.Default.taggedFunction6Color;
+            taggedFuncLabel6.Font = Settings.Default.taggedFunction6Font ?? _defaultTreeFont;
+
+            numericUpDownMaxParametersLength.Value = Settings.Default.MaxParametersLength;
+            numericUpDownMaxParametersLengthInFunctionChain.Value = Settings.Default.MaxParametersLengthInFunctionChain;
+
+            chCheckForVersionUpdates.Checked = Settings.Default.CheckForVersionUpdates;
+            chSendStatistics.Checked = Settings.Default.SendStatistics;
+        }
+
+        public void SaveSettings()
+        {
+            Settings.Default.TrackActiveItem = chTrackActiveItem.Checked;
+            Settings.Default.Extensions = new System.Collections.Specialized.StringCollection();
+            Settings.Default.Extensions.AddRange(edExtensions.Lines);
+
+            Settings.Default.taggedFunction1Color = taggedFuncLabel1.ForeColor;
+            Settings.Default.taggedFunction1Font = taggedFuncLabel1.Font;
+            Settings.Default.taggedFunction2Color = taggedFuncLabel2.ForeColor;
+            Settings.Default.taggedFunction2Font = taggedFuncLabel2.Font;
+            Settings.Default.taggedFunction3Color = taggedFuncLabel3.ForeColor;
+            Settings.Default.taggedFunction3Font = taggedFuncLabel3.Font;
+            Settings.Default.taggedFunction4Color = taggedFuncLabel4.ForeColor;
+            Settings.Default.taggedFunction4Font = taggedFuncLabel4.Font;
+            Settings.Default.taggedFunction5Color = taggedFuncLabel5.ForeColor;
+            Settings.Default.taggedFunction5Font = taggedFuncLabel5.Font;
+            Settings.Default.taggedFunction6Color = taggedFuncLabel6.ForeColor;
+            Settings.Default.taggedFunction6Font = taggedFuncLabel6.Font;
+
+            Settings.Default.MaxParametersLength = Convert.ToInt32(numericUpDownMaxParametersLength.Value);
+            Settings.Default.MaxParametersLengthInFunctionChain = Convert.ToInt32(numericUpDownMaxParametersLengthInFunctionChain.Value);
+
+            Settings.Default.CheckForVersionUpdates = chCheckForVersionUpdates.Checked;
+            Settings.Default.SendStatistics = chSendStatistics.Checked;
+
+            Settings.Default.Save();
+        }
+
+        private void btnForceSendStatistics_Click(object sender, EventArgs e)
+        {
+            StatisticsManager.Instance.SubmitStatisticsToServer(force: true);
+        }
+
+        private void ShowFontDialogForLabel(string lblName)
+        {
+            Label lbl = (Label)Controls.Find(lblName, true).First();
+            fontDialog1.AllowScriptChange = false;
+            fontDialog1.AllowVerticalFonts = false;
+            fontDialog1.ShowColor = true;
+            fontDialog1.ShowEffects = true;
+            fontDialog1.FontMustExist = true;
+
+            fontDialog1.Font = lbl.Font;
+            fontDialog1.Color = lbl.ForeColor;
+
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lbl.Font = fontDialog1.Font;
+                lbl.ForeColor = fontDialog1.Color;
+            }
+        }
+
+        private void ShowColorDialogForLabel(string lblName)
+        {
+            Label lbl = (Label)Controls.Find(lblName, true).First();
+            colorDialog1.Color = lbl.ForeColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lbl.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            var lblName = "taggedFuncLabel" + (string)((Control)sender).Tag;
+            ShowFontDialogForLabel(lblName);
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            var lblName = "taggedFuncLabel" + (string)((Control)sender).Tag;
+            ShowColorDialogForLabel(lblName);
+        }
+    }
+}
--- source/JsParserCore/UI/SettingsForm.cs
+++ source/JsParserCore/UI/SettingsForm.cs
@@ -13,163 +13,21 @@ namespace JsParserCore.UI
 {
 	public partial class SettingsForm : Form
 	{
-		private Font _defaultTreeFont;
-
 		public SettingsForm(Font defaultTreeFont)
 		{
-			_defaultTreeFont = defaultTreeFont;
 			InitializeComponent();
+			jsParserSettingsControl1.InitSettings();
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			Settings.Default.TrackActiveItem = chTrackActiveItem.Checked;
-			Settings.Default.Extensions = new System.Collections.Specialized.StringCollection();
-			Settings.Default.Extensions.AddRange(edExtensions.Lines);
-
-			Settings.Default.taggedFunction1Color = taggedFuncLabel1.ForeColor;
-			Settings.Default.taggedFunction1Font = taggedFuncLabel1.Font;
-			Settings.Default.taggedFunction2Color = taggedFuncLabel2.ForeColor;
-			Settings.Default.taggedFunction2Font = taggedFuncLabel2.Font;
-			Settings.Default.taggedFunction3Color = taggedFuncLabel3.ForeColor;
-			Settings.Default.taggedFunction3Font = taggedFuncLabel3.Font;
-			Settings.Default.taggedFunction4Color = taggedFuncLabel4.ForeColor;
-			Settings.Default.taggedFunction4Font = taggedFuncLabel4.Font;
-			Settings.Default.taggedFunction5Color = taggedFuncLabel5.ForeColor;
-			Settings.Default.taggedFunction5Font = taggedFuncLabel5.Font;
-			Settings.Default.taggedFunction6Color = taggedFuncLabel6.ForeColor;
-			Settings.Default.taggedFunction6Font = taggedFuncLabel6.Font;
-
-			Settings.Default.MaxParametersLength = Convert.ToInt32(numericUpDownMaxParametersLength.Value);
-			Settings.Default.MaxParametersLengthInFunctionChain = Convert.ToInt32(numericUpDownMaxParametersLengthInFunctionChain.Value);
-
-			Settings.Default.CheckForVersionUpdates = chCheckForVersionUpdates.Checked;
-			Settings.Default.SendStatistics = chSendStatistics.Checked;
-
-			Settings.Default.Save();
+			jsParserSettingsControl1.SaveSettings();
 			Close();
-		}
-
-		private void SettingsForm_Load(object sender, EventArgs e)
-		{
-			chTrackActiveItem.Checked = Settings.Default.TrackActiveItem;
-			edExtensions.Lines = Settings.Default.Extensions.OfType<string>().ToArray();
-
-			taggedFuncLabel1.ForeColor = Settings.Default.taggedFunction1Color;
-			taggedFuncLabel1.Font = Settings.Default.taggedFunction1Font ?? _defaultTreeFont;
-			taggedFuncLabel2.ForeColor = Settings.Default.taggedFunction2Color;
-			taggedFuncLabel2.Font = Settings.Default.taggedFunction2Font ?? _defaultTreeFont;
-			taggedFuncLabel3.ForeColor = Settings.Default.taggedFunction3Color;
-			taggedFuncLabel3.Font = Settings.Default.taggedFunction3Font ?? _defaultTreeFont;
-			taggedFuncLabel4.ForeColor = Settings.Default.taggedFunction4Color;
-			taggedFuncLabel4.Font = Settings.Default.taggedFunction4Font ?? _defaultTreeFont;
-			taggedFuncLabel5.ForeColor = Settings.Default.taggedFunction5Color;
-			taggedFuncLabel5.Font = Settings.Default.taggedFunction5Font ?? _defaultTreeFont;
-			taggedFuncLabel6.ForeColor = Settings.Default.taggedFunction6Color;
-			taggedFuncLabel6.Font = Settings.Default.taggedFunction6Font ?? _defaultTreeFont;
-
-			numericUpDownMaxParametersLength.Value = Settings.Default.MaxParametersLength;
-			numericUpDownMaxParametersLengthInFunctionChain.Value = Settings.Default.MaxParametersLengthInFunctionChain;
-
-			chCheckForVersionUpdates.Checked = Settings.Default.CheckForVersionUpdates;
-			chSendStatistics.Checked = Settings.Default.SendStatistics;
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
 			Close();
-		}
-
-		private void button3_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel1);
-		}
-
-		private void ShowFontDialogForLabel(Label lbl)
-		{
-			fontDialog1.AllowScriptChange = false;
-			fontDialog1.AllowVerticalFonts = false;
-			fontDialog1.ShowColor = true;
-			fontDialog1.ShowEffects = true;
-			fontDialog1.FontMustExist = true;
-
-			fontDialog1.Font = lbl.Font;
-			fontDialog1.Color = lbl.ForeColor;
-
-			if (fontDialog1.ShowDialog() == DialogResult.OK)
-			{
-				lbl.Font = fontDialog1.Font;
-				lbl.ForeColor = fontDialog1.Color;
-			}
-		}
-
-		private void ShowColorDialogForLabel(Label lbl)
-		{
-			colorDialog1.Color = lbl.ForeColor;
-			if (colorDialog1.ShowDialog() == DialogResult.OK)
-			{
-				lbl.ForeColor = colorDialog1.Color;
-			}
-		}
-
-		private void button4_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel2);
-		}
-
-		private void button5_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel3);
-		}
-
-		private void button6_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel4);
-		}
-
-		private void button7_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel5);
-		}
-
-		private void button8_Click(object sender, EventArgs e)
-		{
-			ShowFontDialogForLabel(taggedFuncLabel6);
-		}
-
-		private void button9_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel1);
-		}
-
-		private void button10_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel2);
-		}
-
-		private void button12_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel3);
-		}
-
-		private void button11_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel4);
-		}
-
-		private void button14_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel5);
-		}
-
-		private void button13_Click(object sender, EventArgs e)
-		{
-			ShowColorDialogForLabel(taggedFuncLabel6);
-		}
-
-		private void btnForceSendStatistics_Click(object sender, EventArgs e)
-		{
-			StatisticsManager.Instance.SubmitStatisticsToServer(force: true);
 		}
 	}
 }
