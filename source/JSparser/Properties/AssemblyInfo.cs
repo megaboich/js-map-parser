64233068059b842c87cb47208be47683d5fd5568
mega.boich@gmail.com <mega.boich@gmail.com@a7d6c96a-3943-11de-a15a-d1801d6fabe8>
2012-02-13 14:35:49 +0000

    Prepare new version for publish
    git-svn-id: https://js-addin.googlecode.com/svn@141 a7d6c96a-3943-11de-a15a-d1801d6fabe8

--- source/JSparser/Properties/AssemblyInfo.cs
+++ source/JSparser/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.3.*")]
+[assembly: AssemblyVersion("3.4.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
