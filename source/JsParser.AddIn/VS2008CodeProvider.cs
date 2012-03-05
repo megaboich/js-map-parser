efbe0fe753b05a40d402cd14dfb83220ca5b0455
mega.boich@gmail.com <mega.boich@gmail.com@a7d6c96a-3943-11de-a15a-d1801d6fabe8>
2012-03-05 11:06:09 +0000

    Add new project for VS2008 AddIn
    git-svn-id: https://js-addin.googlecode.com/svn@171 a7d6c96a-3943-11de-a15a-d1801d6fabe8

---
+++ source/JsParser.AddIn/VS2008CodeProvider.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JsParserCore.Code;
+using EnvDTE80;
+using EnvDTE;
+
+namespace JsParser.AddIn
+{
+	public class VS2008CodeProvider : ICodeProvider
+	{
+		private DTE2 _applicationObject;
+		private Document _activeDocument;
+
+		public VS2008CodeProvider(DTE2 applicationObject, Document activeDocument)
+		{
+			_applicationObject = applicationObject;
+			_activeDocument = activeDocument ?? _applicationObject.ActiveDocument;
+			ContainerName = "Visual Studio " + _applicationObject.DTE.Version;
+		}
+
+		private Document Doc
+		{
+			get
+			{
+				return _activeDocument;
+			}
+		}
+
+		#region ICodeProvider Members
+
+		public string LoadCode()
+		{
+			try
+			{
+				var textDocument = (TextDocument)Doc.Object("TextDocument");
+				var docContent = textDocument.CreateEditPoint(textDocument.StartPoint).GetText(textDocument.EndPoint);
+				return docContent;
+			}
+			catch
+			{
+				return "function Error_Loading_Document(){}";
+			}
+		}
+
+		public string Path
+		{
+			get { return Doc != null ? Doc.Path : string.Empty; }
+		}
+
+		public string Name
+		{
+			get { return Doc != null ? Doc.Name : string.Empty; }
+		}
+
+		public void SelectionMoveToLineAndOffset(int StartLine, int StartColumn)
+		{
+			try
+			{
+				var textDocument = (TextDocument)Doc.Object("TextDocument");
+				textDocument.Selection.MoveToLineAndOffset(StartLine, StartColumn, false);
+			}
+			catch
+			{
+			}
+		}
+
+		public void SetFocus()
+		{
+			if (Doc == null)
+			{
+				return;
+			}
+
+			Doc.Activate();
+			_applicationObject.ActiveWindow.SetFocus();
+		}
+
+		public void GetCursorPos(out int line, out int column)
+		{
+			try
+			{
+				var textDocument = (TextDocument)Doc.Object("TextDocument");
+				line = textDocument.Selection.ActivePoint.Line;
+				column = textDocument.Selection.ActivePoint.DisplayColumn;
+			}
+			catch
+			{
+				line = -1;
+				column = -1;
+			}
+		}
+
+		#endregion
+
+
+		public string ContainerName {get; set;}
+	}
+}
