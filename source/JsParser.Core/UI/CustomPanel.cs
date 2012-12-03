61fa33bd9f321c851bfbef70b12ec42e40b4f54f
mega.boich <mega.boich@gmail.com>
2012-12-03 18:01:53 +0200

    [Implement] Render mode for VS2012 - add backward render setting, improve quality of render, change todo list control

---
+++ source/JsParser.Core/UI/CustomPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace JsParser.Core.UI
+{
+	public class CustomPanel : Panel
+	{
+        public CustomPanel()
+            : base()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+	}
+}
