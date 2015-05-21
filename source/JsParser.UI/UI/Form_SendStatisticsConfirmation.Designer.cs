ad4711fbee0feab5615d9eb440d3d646281012e7
Olek <mega.boich@gmail.com>
2015-05-21 11:27:24 +0300

    Fix size of  statistics confirmation dialog

--- source/JsParser.UI/UI/Form_SendStatisticsConfirmation.Designer.cs
+++ source/JsParser.UI/UI/Form_SendStatisticsConfirmation.Designer.cs
@@ -40,37 +40,36 @@
             // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.BackColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(112, 60);
+            this.label1.Location = new System.Drawing.Point(112, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(372, 93);
-            this.label1.TabIndex = 0;
+            this.label1.Size = new System.Drawing.Size(372, 94);
+            this.label1.TabIndex = 2;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // checkBox1
             // 
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(65, 177);
+            this.checkBox1.Location = new System.Drawing.Point(115, 107);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(399, 17);
+            this.checkBox1.Size = new System.Drawing.Size(271, 33);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "I am agree to send anonymous statistics related to JavaScript Parser Extension.";
+            this.checkBox1.Text = "I agree to send anonymous statistics related to usage of JavaScript Parser Extens" +
+    "ion.";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(113, 219);
+            this.button1.Location = new System.Drawing.Point(409, 155);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 25);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 0;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -79,10 +78,10 @@
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Ignore;
-            this.button2.Location = new System.Drawing.Point(245, 219);
+            this.button2.Location = new System.Drawing.Point(257, 155);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(146, 25);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 4;
             this.button2.Text = "I\'m not sure, ask me later";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -90,11 +89,11 @@
             // label2
             // 
             this.label2.BackColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(112, 21);
+            this.label2.Location = new System.Drawing.Point(112, 11);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(275, 23);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Help improve Javascript Parser Extension!";
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Help to improve Javascript Parser Extension!";
             // 
             // pictureBox1
             // 
@@ -111,8 +110,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
-            this.ClientSize = new System.Drawing.Size(496, 262);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(496, 192);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
@@ -129,7 +127,6 @@
             this.Text = "JavaScript Parser Extension";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
