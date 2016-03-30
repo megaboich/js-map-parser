4ff7e9aad5e87765651ee4bf50de14d92b253917
mega.boich@gmail.com <mega.boich@gmail.com>
2016-03-30 16:52:44 +0200

    Fix unit tests

--- source/JsParser.Test/Themes/ThemeProviderTest.cs
+++ source/JsParser.Test/Themes/ThemeProviderTest.cs
@@ -15,10 +15,10 @@ namespace JsParser.Test.Themes
         {
             var tp = ThemeProvider.Deserialize("something wrong");
             var themes = tp.GetThemes();
-            Assert.AreEqual(1, themes.Count());
+            Assert.AreEqual(3, themes.Count());
 
             var defTheme = themes.First();
-            Assert.AreEqual("Blue", defTheme.Name);
+            Assert.AreEqual("Light", defTheme.Name);
         }
 
         [Test]
@@ -27,12 +27,12 @@ namespace JsParser.Test.Themes
             var tp = ThemeProvider.Deserialize("something wrong");
             tp.AddTheme("new");
             var themes = tp.GetThemes();
-            Assert.AreEqual(2, themes.Count());
+            Assert.AreEqual(4, themes.Count());
 
             var serialized = tp.Serialize();
             var ntp = ThemeProvider.Deserialize(serialized);
 
-            Assert.AreEqual(2, ntp.GetThemes().Count());
+            Assert.AreEqual(4, ntp.GetThemes().Count());
             var serialized2 = ntp.Serialize();
 
             Assert.AreEqual(serialized, serialized2);
